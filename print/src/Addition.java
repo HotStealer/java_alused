d15446af23784c86b112f2675b7d1e2e0b1f15e5
hotstealer <karl.podra@khk.ee>
2019-09-23 09:46:10 +0300

    E6

---
+++ print/src/Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args) {
+        int y = 5;
+        int x = 4;
+        System.out.println(x + "+" + y + "=" + (x+y));
+        int z = 73457;
+        int c = 12888;
+        System.out.println(z + "+" + c + "=" + (z+c));
+    }
+}
