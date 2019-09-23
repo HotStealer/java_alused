dbea64e0c45cd329d2dc5c7cd0ece352ae7efbb8
hotstealer <karl.podra@khk.ee>
2019-09-23 09:42:29 +0300

    E5

---
+++ print/src/Seconds.java
@@ -0,0 +1,10 @@
+public class Seconds {
+    public static void main(String[] args) {
+        int days = 365;
+        int hours = 24;
+        int min = 60;
+        int sec = 60;
+        int seconds = days * hours * min * sec;
+        System.out.println("There are "+ seconds + " seconds in a year");
+    }
+}
