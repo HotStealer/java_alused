31a5b42ef6e11155404aaab413c49dcc15069fad
hotstealer <karl.podra@gmail.com>
2019-09-30 15:29:00 +0300

    E32

---
+++ print/src/SumOfSet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfSet {
+    public static void main(String[] args) {
+        int start = 1;
+
+        System.out.print("Until what? ");
+        Scanner read2 = new Scanner(System.in);
+        int stop = Integer.parseInt(read2.nextLine());
+
+        int count = 0;
+        while (start <= stop) {
+            count += start;
+            start++;
+        }
+        System.out.println("The sum is " + count);
+    }
+}
