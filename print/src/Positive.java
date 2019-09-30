6012e88975c866225ff0ff96b3e8cfc7f61a54d3
hotstealer <karl.podra@gmail.com>
2019-09-30 10:59:02 +0300

    E14

---
+++ print/src/Positive.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Positive {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your age: ");
+        int y = Integer.parseInt(reader.nextLine());
+        if (y > 0){
+            System.out.print("The number is positive");
+        } else {
+            System.out.print("The number is negative");
+        }
+    }
+}
