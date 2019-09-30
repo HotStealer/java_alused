78f87e4a825d0410dcf8c341f8cc9fd4f9dc614e
hotstealer <karl.podra@gmail.com>
2019-09-30 15:13:43 +0300

    E30

---
+++ print/src/UpTo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UpTo {
+    public static void main(String[] args) {
+        int number = 1;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Up to what number: ");
+        int y = Integer.parseInt(reader.nextLine());
+
+        while (number <= y) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
