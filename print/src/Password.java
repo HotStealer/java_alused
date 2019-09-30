0971dae583fadf7a024d9164dca445c330e9df1a
hotstealer <karl.podra@gmail.com>
2019-09-30 14:32:42 +0300

    E22

---
+++ print/src/Password.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner reader = new Scanner(System.in);
+            System.out.print("Type the password: ");
+            String pass = reader.nextLine();
+             if (pass.equals("carrot")) {
+                 System.out.println("Right!");
+                 System.out.println("The secret is: jryy qbar!");
+                 break;
+
+             } else {
+                 System.out.println("Wrong!");
+             }
+        }
+    }
+}
