41a32588a4d77b243859c017c19fd8480f65165e
hotstealer <karl.podra@gmail.com>
2019-09-30 11:30:10 +0300

    E19

---
+++ print/src/Check.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Check {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("How old are you: ");
+        int y = Integer.parseInt(reader.nextLine());
+        if (y < 0 || y > 120){
+            System.out.print("Impossible");
+        } else  {
+            System.out.print("OK");
+        }
+
+    }
+}
