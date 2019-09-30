9639fd38a46fe19d33b95f9434bf21f71ac08fc9
hotstealer <karl.podra@gmail.com>
2019-09-30 15:17:07 +0300

    E31

---
+++ print/src/LowerAndUpperLimit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LowerAndUpperLimit {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("From what number: ");
+        int x = Integer.parseInt(reader.nextLine());
+        System.out.print("Up to what number: ");
+        int y = Integer.parseInt(reader.nextLine());
+        System.out.println("First: " + x);
+        System.out.println("Last: " +y);
+        while (x <= y) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
+
