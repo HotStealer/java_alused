2e56dcb2cded5a05322360e97175f6d06b130d13
hotstealer <karl.podra@gmail.com>
2019-09-30 14:50:31 +0300

    Week 1

--- print/src/Name.java
+++
@@ -1,6 +0,0 @@
-public class Name {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("(and all the people of the world)");
-    }
-}
