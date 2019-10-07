be160ef8285238416d109430b9c20ce82ef89e50
hotstealer <karl.podra@gmail.com>
2019-10-07 12:26:19 +0300

    E44

---
+++ print/src/Least.java
@@ -0,0 +1,11 @@
+public class Least {
+    public static void main(String[] args) {
+        int answer = least(10, 8);
+        System.out.println("Least: " + answer);
+    }
+
+    public static int least(int i, int i1) {
+        return Math.min(i,i1);
+    }
+
+}
