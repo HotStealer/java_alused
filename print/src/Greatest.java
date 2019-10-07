7ffa168e129e918734252f528a8ed402c569ae48
hotstealer <karl.podra@gmail.com>
2019-10-07 12:31:26 +0300

    E45

---
+++ print/src/Greatest.java
@@ -0,0 +1,13 @@
+public class Greatest {
+    public static void main(String[] args) {
+        int answer = greatest(25, 18, 12);
+        System.out.println("Greatest: " + answer);
+    }
+
+    public static int greatest(int i2, int i, int i1) {
+        int first = Math.max(i, i1);
+        int second = Math.max(i1, i2);
+        return Math.max( first,second);
+    }
+
+}
