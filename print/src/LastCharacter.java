691f7404e0f8823423486d4092005a5ac680ac28
hotstealer <karl.podra@gmail.com>
2019-10-07 13:45:45 +0300

    E49

---
+++ print/src/LastCharacter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LastCharacter {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        char letter = lastCharacter(name);
+        System.out.println("Last character: " + letter);
+
+    }
+
+    private static char lastCharacter(String name) {
+        char w = name.charAt(name.length()-1);
+        return w;
+    }
+}
