50a4c24d1fd4d69df3de9cfb04950d5b13dc8a8e
hotstealer <karl.podra@gmail.com>
2019-10-07 11:59:18 +0300

    E40

---
+++ print/src/PrintingBoss.java
@@ -0,0 +1,41 @@
+public class PrintingBoss {
+    public static void main(String[] args) {
+
+        xmasTree(20);
+    }
+
+    public static void printStars(int number) {
+        int i = 0;
+        while (i < number) {
+            System.out.print("*");
+            i++;
+            if (i>1) {
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+    }
+    public static void printWhitespaces(int size) {
+        int i = 0;
+        while (i < size) {
+            System.out.print(" ");
+            i++;
+        }
+        //System.out.println("");
+    }
+    public static void xmasTree(int height) {
+        int i = height;
+        int j = 0;
+        while (i >= 0) {
+            printWhitespaces(height-j);
+            printStars(height-i);
+            i--;
+            j++;
+        }
+        printWhitespaces(height - 2);
+        printStars(2);
+        printWhitespaces(height -2);
+        printStars(2);
+    }
+
+}
