9f66a12b37822b09fc6d476fa68604c2d260de09
hotstealer <karl.podra@gmail.com>
2019-10-07 13:37:05 +0300

    E48

---
+++ print/src/FirstCharacter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FirstCharacter {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        char letter = firstCharacter(name);
+        System.out.println("First character: "+ letter);
+
+    }
+
+    private static char firstCharacter(String name) {
+        return name.charAt(0);
+    }
+}
