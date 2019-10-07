615af1f36af33a3de8c9547b0a85ddc80dd1b3a6
hotstealer <karl.podra@gmail.com>
2019-10-07 13:58:50 +0300

    E52

---
+++ print/src/ReversingName.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReversingName {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        int i = name.length();
+        while (i > 0) {
+            char letter = name.charAt(i-1);
+            i--;
+            System.out.print(letter);
+        }
+    }
+}
