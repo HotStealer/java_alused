b56b0cba5bcf56b40c0ae9b8aad582ee2e49319c
hotstealer <karl.podra@gmail.com>
2019-10-07 13:50:14 +0300

    E50

---
+++ print/src/SeparatingFirstChar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SeparatingFirstChar {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+
+        String name = reader.nextLine();
+        if(name.length() > 3 ){
+            char character1 = name.charAt(0);
+            char character2 = name.charAt(1);
+            char character3 = name.charAt(2);
+            System.out.println("1. character: " + character1);
+            System.out.println("2. character: " + character2);
+            System.out.println("3. character: " + character3);
+        }
+    }
+}
