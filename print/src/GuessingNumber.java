f92894078c64eeb7fde07f74b4ddc1130302b99d
hotstealer <karl.podra@gmail.com>
2019-10-07 12:16:25 +0300

    E41

---
+++ print/src/GuessingNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GuessingNumber {
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 100);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int guess = Integer.parseInt(reader.nextLine());
+        int count = 0;
+        System.out.println(number);
+        while (true) {
+            if (guess > number) {
+                count++;
+                System.out.println("The number is lesser, guesses made: "+ count);
+                guess = Integer.parseInt(reader.nextLine());
+            } else if (guess < number) {
+                count++;
+                System.out.println("The number is greater, guesses made: "+ count);
+                guess = Integer.parseInt(reader.nextLine());
+            } else {
+                System.out.println("Congratulations, your guess is correct!");
+                break;
+            }
+        }
+
+    }
+}
