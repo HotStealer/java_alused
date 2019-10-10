f173e70cfa51e97986a411006dbc7e9023d3a528
hotstealer <karl.podra@gmail.com>
2019-10-10 09:09:02 +0300

    E54

---
+++ print/src/EndPart.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EndPart {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a word: ");
+        String name = reader.nextLine();
+        int end = name.length();
+        System.out.println("Length of the end part: ");
+        int number = Integer.parseInt(reader.nextLine());
+        int start = name.length() - number;
+        System.out.println(name.substring(start, end));
+    }
+}
