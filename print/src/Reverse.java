0bab40571b522ee718474b78f3490a60d8404047
hotstealer <karl.podra@gmail.com>
2019-10-10 09:36:02 +0300

    E56

---
+++ print/src/Reverse.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static String reverse(String text) {
+        int i = text.length();
+        int a = text.length() - 1;
+        StringBuilder word = new StringBuilder();
+        while (i > 0) {
+            String letter = text.substring(a, i);
+            word.append(letter);
+            i--;
+            a--;
+        }
+        return word.toString();
+    }
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
+    }
+
+}
