d10d9c73c63cfadc72c473f5283c4dd98c46d3bd
hotstealer <karl.podra@gmail.com>
2019-10-10 09:55:58 +0300

    E57

---
+++ print/src/Words.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Words {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+        String help = "";
+        while(true){
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+            if (word.isEmpty()){
+                System.out.println("You typed the following words: ");
+                System.out.print(help);
+                break;
+            } else {
+                words.add(word);
+                help = help + "\n" + word;
+            }
+        }
+    }
+}
