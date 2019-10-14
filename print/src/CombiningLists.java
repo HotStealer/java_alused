4f457a6d9c8130c96bff56005e442b4150ef7db7
hotstealer <karl.podra@gmail.com>
2019-10-14 14:01:43 +0300

    E70

---
+++ print/src/CombiningLists.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class CombiningLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+        list1.add(4);
+        list1.add(3);
+
+        list2.add(5);
+        list2.add(10);
+        list2.add(7);
+
+        combine(list1, list2);
+
+        System.out.println(list2);
+    }
+
+    private static void combine(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        list2.addAll(list1);
+    }
+}
